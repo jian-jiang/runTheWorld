d57bd1d1257d498aecd21e8a42f8ecf8e264e4d6
foreverj <adamjiang0914@gmail.com>
2014-12-23 21:01:31 +0800

    data

--- js/index.js
+++ js/index.js
@@ -8,4 +8,12 @@ $(document).ready(function(){
 			$('#messageInput').val('');
 		}
 	});
+	myDataRef.on('child_added', function(snapshot) {
+        var message = snapshot.val();
+		displayChatMessage(message.name, message.text);
+      });
+      function displayChatMessage(name, text) {
+        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      };
 });
