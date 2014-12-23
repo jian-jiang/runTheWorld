72b29d6ccbc2cd3adc4b2d89b7615b3aa7173a9a
foreverj <adamjiang0914@gmail.com>
2014-12-23 20:54:23 +0800

    firebase test

---
+++ js/index.js
@@ -0,0 +1,13 @@
+(functiton(){
+	$(document).ready(
+		var myDataRef = new Firebase('https://rjuta02bfmx.firebaseio-demo.com/');
+	$('#messageInput').keypress(function(){
+		if(e.keyCode===13){
+			var name=$('#nameInput').val();
+			var text=$('#messageInput').val();
+			myDataRef.push({name: name, text: text});
+			$('#messageInput').val('');
+		}
+	});
+	);
+})()
